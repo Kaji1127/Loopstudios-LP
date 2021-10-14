1d0ab076c47ea32304187a7156867ee259306ffb
K.Kishin <riesgraf.birsack.1226@gmail.com>
2021-10-14 14:50:04 +0900

    edited file

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
+const babel = require('gulp-babel');
 const terser = require('gulp-terser');
 const Fiber = require('fibers');
 const browsersync = require('browser-sync').create();
@@ -23,6 +24,7 @@ function scssTask() {
 // JavaScript Task
 function jsTask() {
 	return src('app/js/script.js', { sourcemaps: true })
+		.pipe(babel({ presets: ['@babel/preset-env'] }))
 		.pipe(terser())
 		.pipe(dest('dist', { sourcemaps: '.' }));
 }
