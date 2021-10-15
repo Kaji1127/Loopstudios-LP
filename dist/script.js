7a1930203ce057163529d11dc41795e9db9ae0b1
K.Kishin <riesgraf.birsack.1226@gmail.com>
2021-10-15 16:09:39 +0900

    remove folder

--- dist/script.js
+++
@@ -1,2 +0,0 @@
-const navMenu=document.getElementById("nav-menu"),navToggle=document.getElementById("nav-toggle"),navClose=document.getElementById("nav-close");navToggle&&navToggle.addEventListener("click",(()=>{navMenu.classList.add("show-menu")})),navClose&&navClose.addEventListener("click",(()=>{navMenu.classList.remove("show-menu")}));
-//# sourceMappingURL=script.js.map
